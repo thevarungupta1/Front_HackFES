519e21c0460fa4d5ea056ceada76a8fa0da9aa04
senthil kumar <senthilkmaar@gmail.com>
2019-07-13 14:53:40 +0530

    final project

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,5 @@
 export const config = {
-  apiUrl: 'http://172.18.2.50:8020/api'
+  // apiUrl: 'http://172.18.2.50:8020/api'
+  // apiUrl:'http://localhost:33200/api'
+  apiUrl: 'https://localhost:44369/api'
   };
